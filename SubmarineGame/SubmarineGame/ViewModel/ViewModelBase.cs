678b43cb8c83a100c4937c03ce81e5493e53e63d
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 20:54:42 +0200

    Add ViewModelBase abstract class to ViewModel

---
+++ SubmarineGame/SubmarineGame/ViewModel/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SubmarineGame.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        protected ViewModelBase() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
