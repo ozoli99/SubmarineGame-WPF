cf75b949e0acb1bad06580de46fcd3aafdfb84a5
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 21:40:44 +0200

    Add fields and implement constructor in App class

--- SubmarineGame/SubmarineGame/App.xaml.cs
+++ SubmarineGame/SubmarineGame/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using SubmarineGame.Model;
+using SubmarineGame.View;
+using SubmarineGame.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace SubmarineGame
 {
@@ -13,5 +17,24 @@ namespace SubmarineGame
     /// </summary>
     public partial class App : Application
     {
+        #region Fields
+
+        private SubmarineGameModel _model;
+        private SubmarineGameViewModel _viewModel;
+        private MainWindow _view;
+        private DispatcherTimer _generatorTimer;
+        private DispatcherTimer _moverTimer;
+        private DispatcherTimer _timer;
+
+        #endregion
+
+        #region Constructor
+
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+
+        #endregion
     }
 }
