5274d53a62985eb163c527e350e89c14cd9cc67d
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 20:45:00 +0200

    Add existing IPersistence interface from WinForms application

---
+++ SubmarineGame/SubmarineGame/Persistence/IPersistence.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SubmarineGame.Persistence
+{
+    public interface IPersistence
+    {
+        List<Shape> Load(string path, ref int gameTime, ref int destroyedMineCount);
+        void Save(string path, List<Shape> mines, Shape submarine, int gameTime, int destroyedMineCount);
+    }
+}
