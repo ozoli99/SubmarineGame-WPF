b142691b6be8f97bd2073bf75959f65454b5f546
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 21:02:36 +0200

    Add and implement DelegateCommand class

---
+++ SubmarineGame/SubmarineGame/ViewModel/DelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SubmarineGame.ViewModel
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public DelegateCommand(Action<object> execute) : this(null, execute) { }
+        public DelegateCommand(Func<object, bool> canExecute, Action<object> execute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                throw new InvalidOperationException("Command execution is disabled.");
+            }
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, EventArgs.Empty);
+        }
+    }
+}
